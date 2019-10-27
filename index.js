9ee6c5b4ca313cb7c0fcd958d43c6b626c5a2d09
Fernando Britto <fernando@siatto.com>
2019-10-26 22:43:10 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end('Hello Node 2!\n');
+var express = require('express');
+var server = express();
+
+//const server = http.createServer((req, res) => {
+//  res.statusCode = 200;
+//  res.end('Hello Node 2!\n');
+//});
+
+server.get('/worker', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'mssqlerp',
+        password: 'novasenha@123',
+        server: 'den1.mssql8.gear.host', 
+        database: 'mssqlerp' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT table_catalog [database], table_schema [schema], table_name [name], table_type [type] FROM INFORMATION_SCHEMA.TABLES', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
 });
 
 server.listen(port, () => {
